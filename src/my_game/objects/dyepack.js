ba9416763f89093140fbd1b3f992da2a1ba2204b
Brendan-alt <brendanfrye2001@gmail.com>
2022-02-26 14:43:25 -0800

    Dyepack hit functionality

--- src/my_game/objects/dyepack.js
+++ src/my_game/objects/dyepack.js
@@ -7,6 +7,7 @@
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 
 import engine from "../../engine/index.js";
+import Oscillate from "../../engine/utils/oscillate.js";
 
 
 class DyePack extends engine.GameObject {
@@ -17,7 +18,10 @@ class DyePack extends engine.GameObject {
         this.isFriendly = friendly;
 
         this.isHitAnimated = false;
-        this.timeOfDeletion = Date.now() + 5000; 
+        this.dyeOscillateX = new Oscillate(4,20,300) //TODO: X-Amplitude of 4, 0.2?
+        this.beforeHitSize = [0, 0];
+
+        this.timeOfDeletion = Date.now() + 5000;
 
         this.mRenderComponent = new engine.SpriteRenderable(spriteTexture);
         this.mRenderComponent.setColor([1, 1, 1, 0]);
@@ -36,24 +40,52 @@ class DyePack extends engine.GameObject {
         //this.toggleDrawRigidShape();
     }
 
-    hit(objectHit) {
-        //TODO: DO SOMETHING
-        if(this.isFriendly) {
-            if (objectHit === 0) {
+    hit(objectType, objectHit = null) { //objectType 0 = patrolBB, 1 = head, 2 = wing, 3 = hero
+        let xform = this.getXform();
+        let isDone = false;
+        if(this.isFriendly) { //If the bullets are fired from the hero
+            //Check if we hit the bounds of patrol, objectType 0, then slow down
+            if (objectType === 0) {
                 this.decreaseSpeedBy(this.kDelta);
             }
-            
-            if (objectHit === 1 || objectHit === 2) {
-                //console.log("Dyepack has been hit!");
-                this.isHitAnimated = true;
-                //Terminate after hit
+            //Check if we hit the a head or wing bounds, objectType 1/2, then check pixelTouches
+            if (objectType === 1 || objectType === 2) {
+                if(!this.isHitAnimated) { //Check if currently animated
+                    if(!objectHit === null) {
+                        if(this.pixelTouches(objectHit)) {
+                            this._OnStart(xform);
+                            //TODO: console.log("TEST");
+                        } //If hit object is defined, check pixel positions
+                    } else {
+                        this._OnStart(xform);
+                    } //otherwise if no object is defined, run hit anyways
+                }
             }
-        } else {
-            // if hit hero object then perform hit, no slow needed
-            if (objectHit === 3) {
-                //???
+        } else { //Otherwise if the bullets are fired from enemies
+            // TODO: if hit hero object then perform hit, no slow needed
+            if (objectType === 3) {
+                if(!this.isHitAnimated) { 
+                    if(!objectHit === null) {
+                        if(this.pixelTouches(objectHit)) {
+                            this._OnStart(xform);
+                        } //If hit object is defined, check pixel positions
+                    }  //otherwise if no object is defined, don't run hit
+                }
             }
         }
+
+        if (this.isHitAnimated) {
+            xform.setPosition(this.beforeHitPos[0], this.beforeHitPos[1]);
+            xform.incXPosBy(this.dyeOscillateX.getNext())
+            isDone = this.dyeOscillateX.done();
+        }
+
+        if(isDone) { //TODO: Redundant?
+            this.isHitAnimated = false;
+            this._OnDelete();
+            //console.log("Despawned due to animation concluding");
+        }
+        return isDone;
     }
 
     NoteSet(set){
@@ -68,8 +100,6 @@ class DyePack extends engine.GameObject {
 
     update(aCamera) {
         let xForm = this.getXform();
-        //Set default movement 
-        //TODO: need to implement checks for other objects...?
         if(!this.isHitAnimated) {
             if(this.isFriendly) {
                 xForm.setPosition(xForm.getXPos() + this.mSpeed, xForm.getYPos());
@@ -79,12 +109,12 @@ class DyePack extends engine.GameObject {
 
             if(this.mSpeed <= 0) {
                 //console.log("Despawned due to lack of speed");
-                this.OnDelete();
+                this._OnDelete();
             }
     
             if(Date.now() >= this.timeOfDeletion) {
                 //console.log("Despawned due to existing too long");
-                this.OnDelete();
+                this._OnDelete();
             }
         }
         
@@ -96,12 +126,18 @@ class DyePack extends engine.GameObject {
         let rightBound = aCamera.getWCCenter()[0] + aCamera.getWCWidth()/2;
         let leftBound = aCamera.getWCCenter()[0] - aCamera.getWCWidth()/2
         if(xForm.getXPos() > rightBound || xForm.getXPos() < leftBound) {
-            this.OnDelete();
+            this._OnDelete();
             //console.log("Despawned due to world edge");
         }
     }
 
-    OnDelete(){
+    _OnStart(xform) {
+        this.beforeHitPos = [xform.getXPos(), xform.getYPos()];
+        this.dyeOscillateX.reStart();
+        this.isHitAnimated = true;
+    }
+
+    _OnDelete(){
         this.set.removeFromSet(this);
         delete this;
     }
