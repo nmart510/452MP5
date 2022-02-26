7acef7f5c1bfe7ffb79011e207a0867dc7417c3d
a-calkins <81609703+a-calkins@users.noreply.github.com>
2022-02-26 14:48:52 -0800

    update zoom cameras

--- src/engine/cameras/zoom_camera.js
+++ src/engine/cameras/zoom_camera.js
@@ -4,6 +4,14 @@ import Camera from "./camera_main.js";
 class ZoomCamera extends Camera {
     constructor(wcCenter, wcWidth, viewportArray = [0,0,200,200], bound) {
         super(wcCenter, wcWidth, viewportArray, bound);
+        this.wasForced = false;
     }
-}
+
+    setForcedVisible(wasForced) {
+        this.wasForced = wasForced;
+    }
+    getForcedVisible() { 
+        return this.wasForced; 
+    }
+} 
 export default ZoomCamera;
