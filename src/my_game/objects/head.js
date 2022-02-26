75d5ca1960f6e3550fe446773b8209513cec6b9f
nmart510 <nicholas.pa.ma@gmail.com>
2022-02-26 14:59:57 -0800

    removed unneeded function

--- src/my_game/objects/head.js
+++ src/my_game/objects/head.js
@@ -51,31 +51,6 @@ class Head extends engine.GameObject {
         this.set = set;
         this.other = other;
     }
-    _createParticle(atX, atY) {
-        let life = 30 + Math.random() * 200;
-        let p = new engine.Particle(engine.defaultResources.getDefaultPSTexture(), atX, atY, life);
-        p.setColor([1, 0, 0, 1]);
-        
-        // size of the particle
-        let r = 5.5 + Math.random() * 0.5;
-        p.setSize(r, r);
-        
-        // final color
-        let fr = 3.5 + Math.random();
-        let fg = 0.4 + 0.1 * Math.random();
-        let fb = 0.3 + 0.1 * Math.random();
-        p.setFinalColor([fr, fg, fb, 0.6]);
-        
-        // velocity on the particle
-        let fx = 10 - 20 * Math.random();
-        let fy = 10 * Math.random();
-        p.setVelocity(fx, fy);
-        
-        // size delta
-        p.setSizeDelta(0.98);
-        
-        return p;
-    }
     hit(){
         //particle emitter
         for (let i = 0; i < 20; i++){
